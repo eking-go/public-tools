a355a15aa2e9075c99f245cb83446a6c6d8406c6
Gatilov Nikolay (work) <eking-go@users.noreply.github.com>
2015-07-01 17:30:02 +0300

    Adaptec 6805 Firmware 5.2-0 (19147) bug fix 	modified:   adaptectemp.py 	modified:   check_raid_temp.py

--- check_raid_temp.py
+++ check_raid_temp.py
@@ -3,13 +3,16 @@
 
 '''
 nagios check Adaptec raid temperature plugin
+On the Adaptec 6805 Firmware 5.2-0 (19147) sometimes arcconf return
+incorrect temperature -4C or empty string. For fix this bug I ask
+the controller again. shit, yes ;(
 Last version: https://github.com/eking-go/public-tools
 '''
 
 __author__ = 'Nikolay Gatilov'
 __copyright__ = 'Nikolay Gatilov'
 __license__ = 'GPL'
-__version__ = '0.1.2015062417'
+__version__ = '0.1.2015070117'
 __maintainer__ = 'Nikolay Gatilov'
 
 import sys
@@ -22,19 +25,23 @@ def get_temp_status():
     ''' return (temp_celsius, temp_F, status)'''
     #   Temperature                              : 40 C/ 104 F (Normal)
     t_re = re.compile('^\s*Temperature\s*:\s*(\d*).*C/\s(\d*).*\((.*)\).*$')
-    p = subprocess.Popen("/usr/bin/sudo /usr/StorMan/arcconf GETCONFIG 1 AL",
-                         shell=True,
-                         stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.STDOUT,
-                         close_fds=True)
-    (s_out, s_err) = (p.stdout, p.stderr)
-    for line in s_out:
-        status = t_re.match(line)
-        if status:
-            return (int(status.group(1)),
-                    int(status.group(2)),
-                    status.group(3))
+    while True:
+        p = subprocess.Popen("/usr/bin/sudo /usr/StorMan/arcconf GETCONFIG 1 AL",
+                             shell=True,
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT,
+                             close_fds=True)
+        (s_out, s_err) = (p.stdout, p.stderr)
+        for line in s_out:
+            status = t_re.match(line)
+            if status:
+                try:
+                    return (int(status.group(1)),
+                            int(status.group(2)),
+                            status.group(3))
+                except:
+                    pass
     return (0, 0, 'FAIL CHECK ARCCONF: %s' % s_err)
 
 
